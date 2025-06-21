3242428511c158ba0a8902ed0036aeb7150aaa87
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2025-06-21 17:30:47 +0100

    landing page update

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,31 +1,30 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 
-
-const SearchBox = ({ history }) => {
+const SearchBox = () => {
   const [keyword, setKeyword] = useState('')
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const submitHandler = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (keyword.trim()) {
-      navigate(`/search/${keyword}`);
+      navigate(`/search/${keyword}`)
     } else {
-      navigate('/');
+      navigate('/')
     }
-  };
-
+  }
 
   return (
-    <Form onSubmit={submitHandler} className='d-flex' inline>
+    <Form onSubmit={submitHandler} className='d-flex'>
       <Form.Control
         type='text'
         name='q'
+        value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
         placeholder='Search Products...'
         className='me-sm-2 ms-sm-5'
-      ></Form.Control>
+      />
       <Button type='submit' variant='outline-light' className='p-2'>
         Search
       </Button>
